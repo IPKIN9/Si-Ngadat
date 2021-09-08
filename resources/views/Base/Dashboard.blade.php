9e2016e27d68421fac2e027398ffe0ae9dbea4b3
IPKIN9 <irwandirini@gmail.con>
2021-09-08 16:30:58 +0700

    Task SN-11

---
+++ resources/views/Base/Dashboard.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon.png') }}">
+    <title>Sistem Informasi Peraturan Lembaga Adat</title>
+    <link href="{{ asset('assets/extra-libs/c3/c3.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/libs/chartist/dist/chartist.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/extra-libs/jvector/jquery-jvectormap-2.0.2.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/dist/css/style.min.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
+    <div id="main-wrapper" data-theme="light" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed" data-boxed-layout="full">
+        <header class="topbar" data-navbarbg="skin6">
+            @include('Base.Headbar')
+        </header>
+        <aside class="left-sidebar" data-sidebarbg="skin6">
+            @include('Base.SideNavbar')
+        </aside>
+        <div class="page-wrapper">
+            <div class="page-breadcrumb">
+                <div class="row">
+                    <div class="col-7 align-self-center">
+                        <h3 class="page-title text-truncate text-dark font-weight-medium mb-1">Good Morning Jason!</h3>
+                        <div class="d-flex align-items-center">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb m-0 p-0">
+                                    <li class="breadcrumb-item"><a href="index.html">Dashboard</a>
+                                    </li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="col-5 align-self-center">
+                        <div class="customize-input float-right">
+                            <select
+                                class="custom-select custom-select-set form-control bg-white border-0 custom-shadow custom-radius">
+                                <option selected>Aug 19</option>
+                                <option value="1">July 19</option>
+                                <option value="2">Jun 19</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                @yield('content')
+            </div>
+            <footer class="footer text-center text-muted">
+                All Rights Reserved by Adminmart. Designed and Developed by <a
+                    href="https://wrappixel.com">WrapPixel</a>.
+            </footer>
+        </div>
+    </div>
+    <script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/popper.js/dist/umd/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/dist/js/app-style-switcher.js') }}"></script>
+    <script src="{{ asset('assets/dist/js/feather.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/dist/js/sidebarmenu.js') }}"></script>
+    <script src="{{ asset('assets/dist/js/custom.min.js') }}"></script>
+    <script src="{{ asset('assets/extra-libs/c3/d3.min.js') }}"></script>
+    <script src="{{ asset('assets/extra-libs/c3/c3.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/chartist/dist/chartist.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js') }}"></script>
+    <script src="{{ asset('assets/extra-libs/jvector/jquery-jvectormap-2.0.2.min.js') }}"></script>
+    <script src="{{ asset('assets/extra-libs/jvector/jquery-jvectormap-world-mill-en.js') }}"></script>
+    <script src="{{ asset('assets/dist/js/pages/dashboards/dashboard1.min.js') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/Base/SideNavbar.blade.php
@@ -0,0 +1,10 @@
+<div class="scroll-sidebar" data-sidebarbg="skin6">
+    <nav class="sidebar-nav">
+        <ul id="sidebarnav">
+            <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="#" aria-expanded="false"><i
+                        data-feather="home" class="feather-icon"></i><span class="hide-menu">Dashboard</span></a></li>
+            <li class="list-divider"></li>
+            <li class="nav-small-cap"><span class="hide-menu">Applications</span></li>
+        </ul>
+    </nav>
+</div>
